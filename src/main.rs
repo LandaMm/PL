2760b0e5ae7019208eb9a7afc69399c1233b4143
LandaMm <qwer.009771@gmail.com>
2023-11-28 19:04:51 +0100

    test: functions

--- src/main.rs
+++ src/main.rs
@@ -4,10 +4,18 @@ mod lexer;
 mod macros;
 
 fn main() {
-    let source = "(56.23546)+/*-#=s_ome4";
+    let source = "
+# functions
+def mul(a, b) {
+  x = a * b
+  return x
+}
+
+mul(5, 10)
+    ";
     let mut lexer = Lexer::new(source.to_string());
     match lexer.tokenize() {
         Err(err) => panic!("Error: {}", err),
-        Ok(_) => println!("tokens: {:?}", lexer.tokens),
+        Ok(_) => println!("tokens: {:#?}", lexer.tokens),
     }
 }
