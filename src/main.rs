01cea89a9e975f8db1953d97ee68d9f1b0e2cc5c
LandaMm <qwer.009771@gmail.com>
2023-12-07 21:54:45 +0100

    feat: call parser

--- src/main.rs
+++ src/main.rs
@@ -2,11 +2,16 @@ use std::fs;
 
 use lexer::Lexer;
 
+use crate::{
+    lexer::token::{Token, TokenKind},
+    parser::ast::Parser,
+};
+
 // use crate::lexer::token::TokenKind;
 
 mod lexer;
 mod macros;
-// mod parser;
+mod parser;
 
 fn read_file(file_path: String) -> String {
     let contents = fs::read_to_string(file_path).expect("Should have been able to read the file");
@@ -15,23 +20,22 @@ fn read_file(file_path: String) -> String {
 }
 
 fn main() {
-    let source = read_file("test.pl".to_string());
+    let source = read_file("test/test-variable-declaration.pl".to_string());
     let mut lexer = Lexer::new(source.to_string());
     match lexer.tokenize() {
         Err(err) => panic!("Error: {}", err),
         Ok(_) => {
             println!("tokens: {:#?}", lexer.tokens);
-            // let mut parser = Parser::new(
-            //     lexer
-            //         .tokens
-            //         .into_iter()
-            //         .filter(|tok| !matches!(tok, TokenKind::Newline))
-            //         .collect(),
-            // );
-            // match parser.produce_ast() {
-            //     Ok(program) => println!("ast: {:#?}", program),
-            //     Err(err) => panic!("Error while parsing: {}", err),
-            // }
+            let with_no_newline: Vec<Box<dyn Token>> = lexer
+                .tokens
+                .into_iter()
+                .filter(|tok| !matches!(tok.kind(), TokenKind::Newline))
+                .collect();
+            let mut parser = Parser::new(with_no_newline);
+            match parser.produce_ast() {
+                Ok(program) => println!("ast: {:#?}", program),
+                Err(err) => panic!("Error while parsing: {}", err),
+            }
         }
     }
 }
