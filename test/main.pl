195e342a53f54bc196942361594cf9c36b302482
LandaMm <qwer.009771@gmail.com>
2023-12-09 15:11:13 +0100

    test: basic object code

--- test/main.pl
+++ test/main.pl
@@ -1,60 +1,12 @@
-# variables and base types
-let num = 45 # integer
-let floating_num = 403.54 # float
-let my_string = "Hello world!" # string
-let shop_list = ["Carrot", "Apple", "Milk"] # list
-# let my_obj = { "name": "John", "age": 23 }
-let is_adult = true # boolean
 
-# constants
-const PI = 3.1415
-const IS_DARWIN = False
+import wheels.Wheel
 
-const browserWindow = null
+const wheel = Wheel(WHEEL_PORT)
 
-# basic operations
-let y = 5 + 10 - (3 * 10) - (50 / 10)
-y += 56
-y -= 12
-y *= 5
-y /= 10
-y %= 5
-let a = 21 % 10    # a = 1
-
-# functions
-def mul(a, b) {
-  x = a * b
-  return x
+fn start() {
+  wheel.speed = 0
 }
 
-let val = mul(3, 10)   # 30
-
-# if-else statement
-let y
-let t
-
-if x == a and a != 0 {
-  y = x
-} else if a > 0 or a == -5 {
-  y = -x
-} else {
-  y = 0
-  y += a
-}
-
-if is_true {
-  t = 1
-} else {
-  t = 0
-}
-
-# for-loops
-for i in ["first", "second", "third"] {
-  y = i
-}
-
-let a
-
-for i in range(0, 10) {
-  a = i
+fn update(delta) {
+  wheel.speed += 1
 }
